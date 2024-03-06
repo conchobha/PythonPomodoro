527d60f0f1e788e2262767839c4c00ff976cd009
LocalNeckbeard <118300270+LocalNeckbeard@users.noreply.github.com>
2024-03-06 15:32:33 -0500

    Wrote Comments

---
+++ pomo.py
@@ -0,0 +1,7 @@
+#Will First design a simple gui with a programable timer, from there, we can modifiy the code form there
+#First version of this code is from https://pythongeeks.org/python-countdown-clock-timer/
+#Will heavily modify for the final version
+
+#Imports 
+#----------------
+
